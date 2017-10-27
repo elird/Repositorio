2a56f4c504487495abebd149a948838ef70c4def
elird <edgar.lird@gmail.com>
2017-10-26 22:12:10 -0300

    2 comit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'faq/index'
+
 #rutas categoria
   get 'category/index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019155834) do
+ActiveRecord::Schema.define(version: 20171026234728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20171019155834) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image"
   end
 
   add_index "articles", ["category_id"], name: "index_articles_on_category_id", using: :btree
